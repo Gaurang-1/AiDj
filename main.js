032fbfd9dfae0d80f41dce3b0ccc735758b732d3
Gaurang-1 <72252176+Gaurang-1@users.noreply.github.com>
2021-03-27 16:23:13 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
 song="";
 
+leftWristX=0;
+leftWristY=0;
+
+rightWristx=0;
+rightWristy=0;
+
 function preload(){
     song = loadSound("music.mp3");
 }
@@ -10,6 +16,27 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet=ml5.poseNet(video,modalLoaded);
+    poseNet.on('pose',gotPoses);
+}
+
+function modalLoaded(){
+    console.log("PoseNet is Initialized");
+}
+
+function gotPoses(results){
+    if (results.length > 0){
+        console.log(results);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        leftWristY=results[0].pose.leftWrist.y;
+        console.log("left wrist x ="+leftWristX+"left wrist y ="+leftWristY);
+
+        rightWristX=results[0].pose.rightWrist.x;
+        rightWristY=results[0].pose.rightWrist.y;
+        console.log("right Wrist x ="+rightWristX+"right wrist y ="+rightWristY);
+    }
 }
 
 function draw(){
@@ -18,4 +45,6 @@ function draw(){
 
 function play(){
     song.play();
+    song.setVolume(1);
+    song.rate(1);
 }
